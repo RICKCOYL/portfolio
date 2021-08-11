cbab0f55ec3047c66ae2ea8e964804ceb94212b5
RICKCOYL <rickoburu@gmail.com>
2021-08-11 20:31:06 +0300

    remove project

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load("particles-js", "assets/js/particles.json", function () {
-    console.log("callback - particles.js config loaded");
-  });
-});
